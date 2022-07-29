c5b150838ceb5f095101e50e8425eb2438276e2c
Hara <harachung90@gmail.com>
2022-07-29 21:13:15 +1000

    Initial commit. Step 1 - 6.

---
+++ src/DNA.java
@@ -0,0 +1,22 @@
+// this program determines whether there is a protein in a strand of DNA.
+public class DNA {
+
+    public static void main(String[] args) {
+
+        //  -. .-.   .-. .-.   .
+        //    \   \ /   \   \ /
+        //   / \   \   / \   \
+        //  ~   `-~ `-`   `-~ `-
+
+        String dna1 = "ATGCGATACGCTTGA";
+        String dna2 = "ATGCGATACGTGA";
+        String dna3 = "ATTAATATGTACTGA";
+
+        String dna = dna1;
+
+        // check the length of the dna string
+        System.out.println(dna1.length());
+
+    }
+
+}
